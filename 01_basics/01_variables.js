8f67aced5b1d1bdc82bca6d191e7ca629a3d78c6
validme <91865927+validme@users.noreply.github.com>
2024-03-05 05:41:57 +0000

    configuration about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 3432423
+let accountEmail = "abc@gmaail.com"
+var accountPassword = "22222"
+let accountState;
+
+/*
+Prefer not to use variable var
+Coz of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountState])
