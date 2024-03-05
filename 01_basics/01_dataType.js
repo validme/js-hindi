1288964855fbd9fefd86edff11d5b1540141aad5
validme <91865927+validme@users.noreply.github.com>
2024-03-05 06:09:06 +0000

    config data type

---
+++ 01_basics/01_dataType.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we're using nodejs, not browser
+
+console.log(3 + 3)
+console("asif")
+
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+
+//object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
